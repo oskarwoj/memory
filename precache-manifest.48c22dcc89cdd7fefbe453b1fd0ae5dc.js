fb836d463e64480bda43d95cbe16554a274f1247
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-08-18 09:46:27 +0200

    Updates

---
+++ precache-manifest.48c22dcc89cdd7fefbe453b1fd0ae5dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3730c9594d751a6a8e9d753f26861d05",
+    "url": "/memory/index.html"
+  },
+  {
+    "revision": "cfb9c01afc475ad27374",
+    "url": "/memory/static/css/main.9d497717.chunk.css"
+  },
+  {
+    "revision": "c933e232b6d6919b9a03",
+    "url": "/memory/static/js/2.f99a1689.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory/static/js/2.f99a1689.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfb9c01afc475ad27374",
+    "url": "/memory/static/js/main.f82679f0.chunk.js"
+  },
+  {
+    "revision": "62e0eae13c9afaa66ed5",
+    "url": "/memory/static/js/runtime-main.c4984c0b.js"
+  },
+  {
+    "revision": "093132f02aaf3793e7766ed91ee349fc",
+    "url": "/memory/static/media/back.093132f0.png"
+  },
+  {
+    "revision": "b7cf87a36dfd4e67cf45f1fba24a1d0d",
+    "url": "/memory/static/media/ipad2.b7cf87a3.png"
+  }
+]);
