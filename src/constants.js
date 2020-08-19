fde5eb0b772d1e35f5edc7c1648077125e4f4608
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-08-19 11:55:23 +0200

    music url changed

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const CARDSNUMBER = 16;
 
 export const URL =
-  "https://raw.githubusercontent.com/oskarwoj/memory/master/src/music/super_mario.mp3";
+  "https://raw.githubusercontent.com/oskarwoj/memory/master/src/assets/music/super_mario.mp3";
 
 export const IMGS = [
   "https://github.com/oskarwoj/memory/blob/master/src/assets/images/blue.png?raw=true",
